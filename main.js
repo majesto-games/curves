6b4a70b53f8c29fd82bdaa77de8095b75fc60d94
Niklas Tegnander <bipshark@gmail.com>
2016-03-09 00:25:28 +0100

    Implement collision

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-import { Graphics, Sprite, Texture, autoDetectRenderer, Container, utils } from 'pixi.js'
-import keys from './keys.js' 
-import p2 from 'p2'
+import expect from "expect"
+import { Graphics, Sprite, Texture, autoDetectRenderer, Container, utils } from "pixi.js"
+import keys from "./keys.js"
+import p2 from "p2"
 
 // Remove pesky pixi.js banner from console
 utils._saidHello = true
@@ -15,26 +16,19 @@ player.position.y = window.innerHeight / 2 - (64/2)
 player.endFill()
 // player.anchor = { x: 0.5, y: 0.5 }
 
-const c = new Container()
+const container = new Container()
 
 const graphics = new Graphics()
 
-document.getElementById('game').appendChild(renderer.view)
+container.addChild(graphics)
+
+document.getElementById("game").appendChild(renderer.view)
 
 const rotationSpeed = 0.05
-const moveSpeed = 4
+const moveSpeed = 2
 const keydown = { left: false, right: false }
 
 let fatness = 5
-let trail = []
-let line = new Graphics()
-let tail = new Graphics()
-c.addChild(line)
-c.addChild(tail)
-line.lineStyle(fatness, 0xffff00)
-tail.lineStyle(fatness, 0xffffff)
-line.moveTo(player.x, player.y)
-tail.moveTo(player.x, player.y)
 
 function funColor () {
   return superFunColor(((0x80 + Math.random() * 0x80) << 16) | ((0x80 + Math.random() * 0x80) << 8) | ((0x80 + Math.random() * 0x80)))
@@ -48,30 +42,111 @@ function superFunColor (input) {
 
 let lx = player.x
 let ly = player.y
+let lfatness = fatness
 let color = funColor()
+let skip_tail_ticker = 0
 
-console.log(color.toString(16))
+// const polygonContainsPoint = (point, points) => {
+//   // TODO Check wether the point is in the bounding box of the polygon
 
-function createTrailPoint (x, y) {
+//   let is_inside = false
+
+//   let prev = points[points.length - 1] // Set 'prev' to be the last element in 'points'
+
+//   for (let p of points) {
+//     const max_x = Math.max(p.x, prev.x)
+//     const min_x = Math.min(p.x, prev.x)
+//     const max_y = Math.max(p.y, prev.y)
+//     const min_y = Math.min(p.y, prev.y)
+
+//     if (point.y >= min_y && point.y < max_y && point.x > min_x) { // Check bounding box
+//       if (point.x > max_x) { // If the point is left of the box, it counts as an intersection
+//         is_inside = !is_inside
+//       } else { // Calculate shit with y=kx+m
+//         const line_k = (prev.y - p.y) / (prev.x - p.x)
+//         const start = prev.x < p.x ? prev : p
+//         const line_x = start.x + ((p.y - start.y) / line_k)
+
+//         if (p.x < line_x) {
+//           is_inside = !is_inside
+//         }
+//       }
+//     }
+
+//     prev = p
+//   }
+
+//   return is_inside
+// }
+//
+// expect(polygonContainsPoint({ x: 10, y: 10 },
+//   [{ x: 5, y: 5 }, { x: 5, y: 15 }, { x: 15, y: 15 }, { x: 15, y: 5 }])).toBe(true)
+// expect(polygonContainsPoint({ x: 20, y: 20 },
+//   [{ x: 5, y: 5 }, { x: 5, y: 15 }, { x: 15, y: 15 }, { x: 15, y: 5 }])).toBe(false)
+// expect(polygonContainsPoint({ x: 5, y: 5 },
+//   [{ x: 2, y: 0 }, { x: 7, y: 3 }, { x: 8, y: 5 }, { x: 0, y: 7 }])).toBe(true)
+
+const createPolygon = (point1, point2, thickness1, thickness2) => {
+  const angle = Math.atan2(point2.y - point1.y, point2.x - point1.x)
+  const angle_perp = angle + Math.PI / 2
+
+  return new PIXI.Polygon([
+    point1.x + (Math.cos(angle_perp) * thickness1 / 2),
+    point1.y + (Math.sin(angle_perp) * thickness1 / 2),
+
+    point2.x + (Math.cos(angle_perp) * thickness2 / 2),
+    point2.y + (Math.sin(angle_perp) * thickness2 / 2),
+
+    point2.x - (Math.cos(angle_perp) * thickness2 / 2),
+    point2.y - (Math.sin(angle_perp) * thickness2 / 2),
+
+    point1.x - (Math.cos(angle_perp) * thickness1 / 2),
+    point1.y - (Math.sin(angle_perp) * thickness1 / 2),
+  ])
+}
+
+function chunk(arr, n) {
+  return arr.slice(0,(arr.length+n-1)/n|0).map(function(_,i) { return arr.slice(n*i,n*i+n); });
+}
+
+const createTail = (x, y, fatness) => {
   let r = Math.random()
+  let pol = null
 
-  if (r < 0.9) {
-    line.lineStyle(fatness, color)
-    line.moveTo(lx, ly)
-    line.lineTo(x, y)
+  if (skip_tail_ticker === 0) {
+    if (r < 0.9) {
+      pol = createPolygon({ x, y }, { x: lx, y: ly }, fatness, lfatness)
+    } else {
+      skip_tail_ticker = 3
+    }
+  } else {
+    skip_tail_ticker--
   }
 
   lx = x
   ly = y
+  lfatness = fatness
 
-  color = funColor()
+  return pol
 }
 
-c.addChild(player)
+container.addChild(player)
 
-let trailCounter = 0
+let tail_ticker = 0
 
-const draw = function () {
+let polygon_tail = []
+
+const im = new PIXI.interaction.InteractionManager(renderer)
+
+let running = true
+let game_over = false
+
+const text = new PIXI.Text("GAME OVER", { font: "64px Impact", fill: "red" })
+
+text.x = window.innerWidth / 2 - text.width / 2
+text.y = window.innerHeight / 2 - text.height / 2
+
+const gameLogic = () => {
   player.x += Math.sin(player.rotation) * moveSpeed
   player.y -= Math.cos(player.rotation) * moveSpeed
 
@@ -81,11 +156,11 @@ const draw = function () {
   if (keys.right.pressed)
     player.rotation = (player.rotation + rotationSpeed) % (2 * Math.PI)
 
-  if (keys.up.pressed)
-    fatness += 0.1
+  // if (keys.up.pressed)
+  //   fatness += 0.1
 
-  if (keys.down.pressed)
-    fatness -= 0.1
+  // if (keys.down.pressed)
+  //   fatness -= 0.1
 
   if (player.rotation < 0)
     player.rotation += 2 * Math.PI
@@ -110,27 +185,53 @@ const draw = function () {
     ly = player.y
   }
 
-  tail.clear()
-  tail.lineStyle(fatness, color)
-  tail.moveTo(player.x, player.y)
-  tail.lineTo(lx, ly)
-
-  if (trailCounter <= 0) {
-    // let x = player.x - Math.sin(player.rotation) * 64
-    // let y = player.y + Math.cos(player.rotation) * 64
-    createTrailPoint(player.x, player.y)
-    trailCounter = Math.floor(fatness)
+  if (polygon_tail.some(poly => poly.contains(player.x, player.y))) {
+    game_over = true
   }
 
-  trailCounter--
+  if (tail_ticker <= 0) {
+    // let x = player.x - Math.sin(player.rotation) * 64
+    // let y = player.y + Math.cos(player.rotation) * 64
+    let p = createTail(player.x, player.y, fatness)
 
-  renderer.render(c)
+    if (p !== null) {
+
+      if (polygon_tail.slice(0, -1).some(poly => chunk(p.points, 2).some(([x, y]) => poly.contains(x, y)))) {
+        game_over = true
+      }
+
+      polygon_tail.push(p)
+      graphics.beginFill(0xffffff, 1)
+      graphics.drawPolygon(p)
+      graphics.endFill()
+    }
+
+    tail_ticker = 2
+  }
+
+  tail_ticker--
+}
+
+const draw = function () {
+  if (!running) return
+
+  if (game_over) {
+    graphics.beginFill(0x000000, 0.85)
+    graphics.drawRect(0, 0, window.innerWidth, window.innerHeight)
+    graphics.endFill()
+    graphics.addChild(text)
+    running = false
+  } else {
+    gameLogic()
+  }
+
+  renderer.render(container)
   requestAnimationFrame(draw)
 }
 
 window.onresize = (e) => {
-  renderer.view.style.width = window.innerWidth + 'px'
-  renderer.view.style.height = window.innerHeight + 'px'
+  renderer.view.style.width = window.innerWidth + "px"
+  renderer.view.style.height = window.innerHeight + "px"
 
   renderer.resize(window.innerWidth, window.innerHeight)
 }
