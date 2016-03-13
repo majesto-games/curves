5f1de814aad01dc56010e5344d294ddb86ca6935
Pierre Krafft <kpierre@outlook.com>
2016-03-13 01:20:06 +0100

    Fix webpack json test for windows and add stage-0 preset

--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,9 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader', query: { presets: ['es2015'] } },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader', query: { presets: ['es2015', 'stage-0'] } },
       { test: /\.json$/, exclude: /node_modules/, loader: 'json' },
-      { test: /\.json$/, include: /node_modules\/(pixi.js|p2)/, loader: 'json' }
+      { test: /\.json$/, include: /node_modules[\/\\](pixi.js|p2)/, loader: 'json' }
     ]
   }
 }
