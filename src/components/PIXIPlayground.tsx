cf6164b06bf7b828d3928d1e642dc1f10362906f
Pierre Krafft <kpierre@outlook.com>
2017-05-22 18:34:57 +0200

    snek in playground

--- src/components/PIXIPlayground.tsx
+++ src/components/PIXIPlayground.tsx
@@ -18,6 +18,11 @@ export default class PIXIPlayground extends React.Component<void, void> {
   private mesh: PIXI.mesh.Mesh
   private i = 0
 
+  private lastPoint = {
+    x: 32,
+    y: 16,
+  }
+
   constructor() {
     super()
     this.renderer = autoDetectRenderer(1200, 800, { antialias: true, backgroundColor: 0x000000 })
@@ -36,8 +41,8 @@ export default class PIXIPlayground extends React.Component<void, void> {
 
     const indicies = new Uint16Array([0, 1])
     this.mesh = new PIXI.mesh.Mesh(createTexture(), vertices, uvs, indicies)
-    this.mesh.x = 10
-    this.mesh.y = 200
+    this.mesh.x = 200
+    this.mesh.y = 300
     this.container.addChild(this.mesh)
 
     this.renderer.render(this.container)
@@ -63,21 +68,28 @@ export default class PIXIPlayground extends React.Component<void, void> {
     // time to add a quad to the end of the tail
     // our usage of triangle_strip should make that very easy
 
-    // let's start by calculating the lower vertice
-    const lx = 32 + this.i * 2
-    const ly = 32
+    const [p1, p2, p3] = nextPoints(this.lastPoint, this.i / 100, 16)
 
-    // then the upper one
-    const hx = 32 + this.i * 2
-    const hy = 0
+    const lx = p2.x
+    const ly = p2.y
+    const hx = p3.x
+    const hy = p3.y
+    this.lastPoint = p1
 
     // now adding both vertices
     const newVerts = new Float32Array([lx, ly, hx, hy])
 
+    // let's start by calculating the lower UV
+    const lxUv = 32 + this.i * 2
+    const lyUv = 32
+
+    // then the upper one
+    const hxUv = 32 + this.i * 2
+    const hyUv = 0
     // Add normalized UV-coordinated
     const newUvs = new Float32Array([
-      lx / TEXTURE_WIDTH, ly / TEXTURE_HEIGHT,
-      hx / TEXTURE_WIDTH, hy / TEXTURE_HEIGHT,
+      lxUv / TEXTURE_WIDTH, lyUv / TEXTURE_HEIGHT,
+      hxUv / TEXTURE_WIDTH, hyUv / TEXTURE_HEIGHT,
     ])
 
     // Add indices referencing the new vertices
@@ -151,4 +163,23 @@ function createTexture() {
   }
   document.body.appendChild(canvas)
   return new Texture(new BaseTexture(canvas))
+}
+
+interface Point {
+  x: number
+  y: number
+}
+
+function nextPoints(point: Point, angle: number, thickness: number) {
+  const x = point.x + Math.cos(angle)
+  const y = point.y - Math.sin(angle)
+  const anglePerp = Math.atan2(point.y - y, point.x - x) + Math.PI / 2
+
+  return [{ x, y}, {
+    x: x - Math.cos(anglePerp) * thickness,
+    y: y - Math.sin(anglePerp) * thickness,
+  }, {
+    x: x + Math.cos(anglePerp) * thickness,
+    y: y + Math.sin(anglePerp) * thickness,
+  }]
 }
